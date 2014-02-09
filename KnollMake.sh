fb993d1379648a8a7f220a03d817caed46f864f3
RabbitHare <philipselectronicmail@gmail.com>
2014-02-08 19:42:06 -0600

    renamed the tileset and the files used to build it

---
+++ KnollMake.sh
@@ -0,0 +1,2 @@
+
+./tset-maker -mesh Knoll.obj -mask Knoll_mask.obj -texture Knoll.tga -tsize 32 32 -linear -name Knoll  Knoll.tset
--- tsetMake.sh
+++
@@ -1,2 +0,0 @@
-
-./tset-maker -mesh tset.obj -mask tset_mask.obj -texture tset.tga -tsize 32 32 -linear -name Knoll  test.tset
